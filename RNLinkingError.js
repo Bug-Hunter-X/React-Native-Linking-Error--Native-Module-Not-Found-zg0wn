d01d2b92f5bb202d706788798ae9c66e3090284b
BugHunter-X <askzenai@gmail.com>
2025-01-08 21:20:05 +0100

    Add RNLinkingError.js file

---
+++ RNLinkingError.js
@@ -0,0 +1,1 @@
+This error occurs when you are using a library that has not been properly linked to your React Native project. This often happens with native modules or libraries that require native code to be compiled and linked.
